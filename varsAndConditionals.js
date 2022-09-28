896e2fe3346401608ebd5479b9b47afd6dabeee8
Luke Passey <ljpassey@gmail.com>
2022-09-28 15:12:17 -0600

    Jamie has attacked Jon twice

---
+++ varsAndConditionals.js
@@ -0,0 +1,38 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+let jonSnowAttack = 25
+let jamieLannisterAttack = 35
+
+if (jonSnowAttack > jamieLannisterAttack) {
+    console.log("Jon Snow has better attack than Jamie Lannister")
+} else if (jamieLannisterAttack > jonSnowAttack) {
+    console.log("Jamie Lannister has better attack than Jon Snow")
+} else {
+    console.log("They are equally matched")
+}
+
+let jonSnowHealth = 100
+let jonSnowDefense = 0
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+    jonSnowHealth = 0
+    console.log("Jon Snow has been slain")
+} else {
+    jonSnowHealth = jonSnowHealth - jamieLannisterAttack
+    //jonSnowHealth -= jamieLannisterAttack
+    console.log(`Jon Snow's Health is down to ${jonSnowHealth}`)
+}
+
+jonSnowDefense += 25
+
+if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
+    console.log("Jon Snow has been slain")
+} else {
+    jonSnowHealth -= (jamieLannisterAttack -jonSnowDefense) 
+    console.log(`Jon Snow's Health is down to ${jonSnowHealth}`)
+}
+
